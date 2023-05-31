f4fae6920a81061897d1a0038d577ffa9a60f6d4
slavpisarov <slav.pisarov@gmail.com>
2023-05-31 11:59:04 +0300

    Add dummy accessories to details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -33,4 +33,8 @@ router.get('/:cubeId/details', async (req,res) => {
     res.render('details', { cube })
 });
 
+router.get('/:cubeId/attach-accessory',(req,res) =>{
+    res.render('accesory/attach')
+})
+
 module.exports = router;
