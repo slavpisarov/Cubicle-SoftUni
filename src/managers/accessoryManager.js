5f3542acaf3c1a174a56ab266633163b382911d8
slavpisarov <slav.pisarov@gmail.com>
2023-05-31 13:38:38 +0300

    Show only available accessories on attach

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -4,3 +4,5 @@ const Accessory = require('../models/Accessory')
 exports.getAll = () => Accessory.find();
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
+
+exports.getExcluded = (accessoryIds) => Accessory.find({_id: {$nin: accessoryIds}});
