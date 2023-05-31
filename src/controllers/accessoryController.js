05b134a4b30df733bde9f9f33e3ea474587cbaa5
slavpisarov <slav.pisarov@gmail.com>
2023-05-31 11:37:00 +0300

    Add post action for create accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -4,5 +4,13 @@ router.get('/create', (req,res) => {
     res.render('accessory/create')
 })
 
+router.post('/create', (req,res) => {
+    const body = req.body;
+
+    //TODO add accessory page to db
+
+    res.redirect('/')
+})
+
 
 module.exports = router;
