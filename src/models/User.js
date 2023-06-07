f65a339ba058d343819697fc56ffad338c9b9927
slavpisarov <slav.pisarov@gmail.com>
2023-06-07 10:59:22 +0300

    Hash password before save

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -10,7 +12,13 @@ userSchema.virtual('repeatPassword')
         if(value !== this.password){
             throw new mongoose.MongooseError('Password missmatch !');
         }
-    })
+    });
+
+userSchema.pre('save', async function(){
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+})
 
 const User = mongoose.model('User', userSchema);
 
